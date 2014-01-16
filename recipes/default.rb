0e61960f097d160794a4453216245c03fd598a2a
Michael Bulat <mbulat@gmail.com>
2014-01-16 17:04:58 +0000

    fix bug where su limit line was added on every chef run

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,10 +13,9 @@ template "/etc/security/limits.conf" do
   variables(:limits => node[:ulimit][:limits])
 end
 
-ruby_block "insert_line" do
-  block do
-    file = Chef::Util::FileEdit.new("/etc/pam.d/su")
-    file.insert_line_if_no_match("/session required pam_limits.so/", "session required pam_limits.so")
-    file.write_file
-  end
+template "/etc/pam.d/su" do
+  mode "0644"
+  owner "root"
+  group "root"
+  source "su.erb"
 end
